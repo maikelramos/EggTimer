428c7ff0985339543f8a7d4243bc90b48e5b2b58
Maikel Ramos <mkramost@gmail.com>
2021-11-12 19:00:28 -0500

    sound and countdown added; project completed

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var countDown: UILabel!
+    var eggTimes = ["Soft":5, "Medium":7,"Hard":12]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    var player: AVAudioPlayer?
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBAction func stateEggPressed(_ sender: UIButton) {
+        
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness+" Egg"
+        countDown.text = String(totalTime)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    @objc func updateTimer(){
+        if secondsPassed < totalTime{
+            let percentageProgress = Float(secondsPassed)/Float(totalTime)
+            progressBar.progress = percentageProgress
+            countDown.text = String(totalTime-secondsPassed)
+            secondsPassed += 1
+        }else {
+            timer.invalidate()
+            titleLabel.text = "DONE!!!"
+            progressBar.progress = 1
+            countDown.text = "0"
+            playSound()
+        }
+    }
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
 
+        player = try! AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+        player?.play()
 
+        
+    }
 }
